617e639e5f4a1ffcf661c9972ed5820ec93dbc75
Sarvar Mumtozbekov <mumtozbekov@gmail.com>
2024-07-17 19:44:52 +0500

    ant design progressbar styles created

---
+++ AntWpf/Converters/StartPointConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace AntWpf.Converters
+{
+    public class StartPointConverter : MarkupExtension, IValueConverter
+    {
+        [Obsolete]
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double v && (v > 0.0))
+            {
+                return new Point(v / 2, 0);
+            }
+
+            return new Point();
+        }
+
+        [Obsolete]
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;    
+        }
+    }
+}
